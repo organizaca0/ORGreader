462d6bee0142a4df419cce85fe0eb9b0a09aa968
Nicolas Rocha Lacerda <nrochalacerda@gmail.com>
2024-11-30 17:09:51 -0300

    feat(splash): adicionado splashscreen com duração de 3 sec
    feat(splash): adicionado splashscreen com duração de 3 sec

--- app/src/main/java/com/example/orgreader/SplashScreen/SplashScreenActivity.kt
+++ app/src/main/java/com/example/orgreader/SplashScreen/SplashScreenActivity.kt
@@ -1,47 +1,26 @@
 package com.example.orgreader.SplashScreen
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.example.orgreader.MainActivity
+import com.example.orgreader.R
 import com.example.orgreader.SplashScreen.ui.theme.ORGreaderTheme
 
 class SplashScreenActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent {
-            ORGreaderTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
-    }
-}
+        setContentView(R.layout.splashscreen)
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ORGreaderTheme {
-        Greeting("Android")
+            finish()
+        }, 3000)
     }
 }
